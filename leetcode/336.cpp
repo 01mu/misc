adb6d6656a56721c91e0cc6be62730b803c6f743
daniel <developmentleviathan@gmail.com>
2019-07-24 19:37:02 -0400

    336

---
+++ leetcode/336.cpp
@@ -0,0 +1,52 @@
+/*
+ * https://leetcode.com/problems/palindrome-pairs/
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int check(string & a, string & b) {
+    string z = a + b;
+
+    int l = z.size();
+    int c = 0;
+
+    for(int i = 0, j = z.size() - 1; i < z.size(); i++, j--) {
+        if(z.at(i) == z.at(j)) {
+            c++;
+        }
+    }
+
+    if(c == l) {
+        return 1;
+    }
+}
+
+int main()
+{
+    vector<string> words{"abcd", "dcba", "lls", "s", "sssll"};
+    //vector<string> words{"bat", "tab", "cat"};
+
+    vector<vector<int>> res;
+
+    for(int i = 0; i < words.size(); i++) {
+        for(int j = 0; j < words.size(); j++) {
+            if(i != j) {
+                if(check(words.at(i), words.at(j)) == 1) {
+                    res.push_back({i, j});
+                }
+            }
+        }
+    }
+
+    for(vector<int> a: res) {
+        for(int val: a) {
+            cout << val << " ";
+        }
+
+        cout << endl;
+    }
+}
