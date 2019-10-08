c0fd1a75902e4a524138b7a5d0fe4bafaa4357e7
daniel <developmentleviathan@gmail.com>
2019-10-08 00:02:45 -0400

    419

--- leetcode/419.cpp
+++ leetcode/419.cpp
@@ -12,23 +12,24 @@ public:
     int countBattleships(vector<vector<char>>& board) {
         int x_len = board[0].size();
         int y_len = board.size();
-
         int count = 0;
+        int z;
+        char ch;
 
         for(int y = 0; y < y_len; y++) {
             for(int x = 0; x < x_len; x++) {
-                char ch = board[y][x] ;
+                ch = board[y][x] ;
 
                 if(ch == 'X') {
                    if(board[y][x+1] == 'X') {
-                        int z = x;
+                        z = x;
 
                         while(board[y][z] == 'X') {
                             board[y][z] = '.';
                             z++;
                         }
                     } else if(y < y_len-1 && board[y+1][x] == 'X') {
-                        int z = y;
+                        z = y;
 
                         do {
                             board[z][x] = '.';
