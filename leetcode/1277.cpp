24601c0ba4d61bae6b4ede142548240a4120c6c8
daniel <developmentleviathan@gmail.com>
2020-01-29 22:43:10 -0500

    1277

---
+++ leetcode/1277.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+
+    }
+};
+
+int main()
+{
+    Solution solution;
+    vector<vector<int>> a{  {0, 1, 1, 1},
+                            {1, 1, 1, 1},
+                            {0, 1, 1, 1}
+                        };
+}
