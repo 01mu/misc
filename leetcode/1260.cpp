7c3c960c2044ebe1450bc442ab68c6bd243acdb5
daniel <developmentleviathan@gmail.com>
2020-02-02 05:07:24 -0500

    1260

---
+++ leetcode/1260.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+private:
+    vector<int> shift(vector<int> & arr, int s) {
+        int len = arr.size(), i, p;
+        vector<int> v;
+
+        if(s > len) {
+            s %= len;
+        }
+
+        p = len-s;
+
+        for(i = p; i < len; i++) {
+            v.push_back(arr[i]);
+        }
+
+        for(i = 0; i < p; i++) {
+            v.push_back(arr[i]);
+        }
+
+        return v;
+    }
+
+    vector<vector<int>> stack(vector<int> & arr, int lvls) {
+        int i, c = 0;
+        vector<vector<int>> v;
+        vector<int> temp;
+
+        for(i = 0; i < arr.size(); i++) {
+            temp.push_back(arr[i]);
+            c++;
+
+            if(c == lvls) {
+                v.push_back(temp);
+                temp.clear();
+                c = 0;
+            }
+        }
+
+        return v;
+    }
+
+    vector<int> flat(vector<vector<int>> & arr) {
+        vector<int> v;
+
+        for(vector<int> a : arr) {
+            for(int i : a) {
+                v.push_back(i);
+            }
+        }
+
+        return v;
+    }
+
+public:
+    vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+        int width = grid[0].size();
+        vector<int> a = flat(grid);
+
+        a = shift(a, k);
+
+        return stack(a, width);
+    }
+};
+
+int main()
+{
+    Solution solution;
+
+    vector<vector<int>> grid{   {1, 2, 3},
+                                {4, 5, 6},
+                                {7, 8, 9}};
+
+    int k = 0;
+
+    vector<vector<int>> stacked = solution.shiftGrid(grid, k);
+
+    for(vector<int> v : stacked) {
+        for(int i: v) {
+            cout << i << " ";
+        }
+
+        cout << endl;
+    }
+
+}
--- leetcode/1286.cpp
+++ leetcode/1286.cpp
@@ -5,40 +5,44 @@ using namespace std;
 
 class CombinationIterator {
 private:
-    int a, b;
-    string ch;
+    vector<string> vals;
+    int idx;
 
 public:
     CombinationIterator(string characters, int combinationLength) {
-        int len = characters.length();
+        int len = characters.length(), i, j;
         string st;
 
         --combinationLength;
 
-        for(int i = 0; i < len; i++) {
-            for(int j = i+1; j < len; j += 1) {
+        for(i = 0; i < len; i++) {
+            for(j = i+1; j < len; j += 1) {
                 st = characters[i] + characters.substr(j, combinationLength);
 
                 if(st.length() > combinationLength) {
-                    cout << i << " " << st << endl;
+                    vals.push_back(st);
                 }
             }
         }
     }
 
     string next() {
-
+        return vals[idx++];
     }
 
     bool hasNext() {
-
+        return idx < vals.size();
     }
 };
 
 int main()
 {
-    string c = "abcdefg";
+    string c = "abcdefgh";
     int cl = 3;
 
     CombinationIterator * obj = new CombinationIterator(c, cl);
+
+    while(obj->hasNext()) {
+        cout << obj->next() << endl;
+    }
 }
