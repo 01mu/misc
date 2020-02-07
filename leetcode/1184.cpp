9f783362c99ee7a9a4477e200fc5165e1dc5f72f
daniel <developmentleviathan@gmail.com>
2020-02-07 03:07:24 -0500

    1184

--- leetcode/1184.cpp
+++ leetcode/1184.cpp
@@ -8,16 +8,14 @@ public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int dest) {
         int i, l = 0, r = 0, b = 0;
 
-        for(i = 0; i < start; i++) {
-            l += distance[i];
-        }
-
-        for(i = dest; i < distance.size(); i++) {
-            r += distance[i];
-        }
-
-        for(i = start; i < dest; i++) {
-            b += distance[i];
+        for(i = 0; i < distance.size(); i++) {
+            if(i >= start && i < dest) {
+                b += distance[i];
+            } else if(i >= dest && i < distance.size()) {
+                r += distance[i];
+            } else if(i >= 0 && i < start) {
+                l += distance[i];
+            }
         }
 
         return min(b, l+r);
@@ -29,7 +27,7 @@ int main()
     Solution solution;
 
     vector<int> distance{1, 2, 3, 4};
-    int start = 0, destination = 3;
+    int start = 0, destination = 1;
 
     cout << solution.distanceBetweenBusStops(distance, start, destination);
 }
