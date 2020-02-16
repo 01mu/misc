7f41fe71dc5e3832b23f553a9231e38f7a93c169
daniel <developmentleviathan@gmail.com>
2020-02-15 19:09:11 -0500

    1347

--- leetcode/1347.cpp
+++ leetcode/1347.cpp
@@ -16,12 +16,15 @@ public:
 
         for(auto & v : a) {
             if(a[v.first] > 0 && b[v.first] == 0) {
-                cout << v.first << ", ";
+                display(v.first, a[v.first]);
                 c++;
-            } else if(a[v.first] > 1 && b[v.first] > 0) {
+            } else if(a[v.first] > 0 && b[v.first] > 0) {
                 z = (a[v.first] - b[v.first]);
-                cout << v.first << " (" << z << "), ";
-                c += z;
+
+                if(z >= 1) {
+                    display(v.first, z);
+                    c += z;
+                }
             }
         }
 
@@ -29,18 +32,28 @@ public:
 
         for(auto & v : b) {
             if(b[v.first] > a[v.first]) {
-                cout << v.first << ", ";
+                display(v.first, b[v.first]-a[v.first]);
             }
         }
 
+        cout << endl;
+
         return c;
+    }
+
+    void display(char ch, int a) {
+        int i;
+
+        for(i = 0; i < a; i ++) {
+            cout << ch << " ";
+        }
     }
 };
 
 int main()
 {
     Solution solution;
-    string s = "friend", t = "family";
+    string s = "anagram", t = "smodoaa";
 
     cout << solution.minSteps(s, t);
 }
