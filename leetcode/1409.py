bb7b0bb2660d99920f0ab4708ba60f309b1cbf1c
daniel <developmentleviathan@gmail.com>
2020-06-02 02:35:12 -0400

    1466

--- leetcode/1409.py
+++ leetcode/1409.py
@@ -7,15 +7,13 @@ class Solution(object):
             v.append(i)
             i += 1
         for i in range(len(queries)):
-            idx = 0
             for j in range(len(v)):
                 if queries[i] == v[j]:
                     v.remove(v[j])
                     a = v
                     v = [queries[i]]
                     v.extend(a)
-                    c.append(idx)
-                idx += 1
+                    c.append(j)
         return c
 
 queries = [3, 1, 2, 1]
